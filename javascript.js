9ba28dbeb6ed6965b44235da2353c828af045fd0
andyfoster <andrewfosteris@gmail.com>
2015-09-23 19:42:37 +1200

    Add responsiveness to image widths

--- javascript.js
+++ javascript.js
@@ -2,21 +2,11 @@ $(document).ready(function() {
   var n = "#nav", no = "nav-open";
   $("#nav-menu").click(function(){
     if ($(n).hasClass(no)) {
-      $(n).animate({height:0}, 300);
-
-      // remove the nasty styling link that will mess up anything else the user tries to do
-      setTimeout(function(){
-        $(n).removeClass(no).removeAttr('style');
-      },320);
-
+      $(n).removeClass(no);
     } else {
-
+      // $(n).addClass(no);
       var newHeight = $(n).css('height', 'auto').height();
-
       $(n).height(0).animate({height:newHeight}, 300);
-      setTimeout(function(){
-        $(n).addClass(no).removeAttr('style');
-      },320);
     }
   });
 });
